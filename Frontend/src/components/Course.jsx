15bd507ef868c92f33909de086ce9c5feb0ea1ff
Vikash <vpal55912@gmail.com>
2024-10-11 23:37:58 +0530

    api url corrected

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -8,7 +8,7 @@ function Course() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get("https://bookstoreapp-id9x.onrender.com/book");
         console.log(res.data);
         setBook(res.data);
       } catch (error) {
